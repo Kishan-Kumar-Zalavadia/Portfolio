0730576b9072bcaa3339e7d3b9d9b325ee4b6395
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-05-02 17:32:18 +0530

    1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,14 @@ import react from "react";
 import styled from "styled-components";
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
+import SocialIcons from "../subComponents/SocialIcons";
+import { NavLink } from 'react-router-dom'
+import { YinYang } from "./AllSvgs";
+import { keyframes } from "styled-components";
+import { useState } from "react";
 
 const MainContainer = styled.div`
-background: ${props=>props.theme.body};
+background: ${props => props.theme.body};
 width: 100vw;
 height: 100vh;
 overflow: hidden;
@@ -14,15 +19,143 @@ h2,h3,h4,h5,h6{
     font-weight: 500;
 }
 `
+
 const Container = styled.div`
 padding: 2rem;
 `
-const Main=()=>{
-    return(
+const Contact = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 2rem;
+right: calc(1rem + 2vh);
+text-decoration: none;
+z-index: 1;
+`
+
+const BLOG = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 50%;
+right: calc(1rem + 2vh);
+transform: rotate(90deg) translate(-50%, -50%);
+text-decoration: none;
+z-index: 1;
+`
+
+const WORK = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 50%;
+left: calc(1rem + 2vh);
+transform: translate(-50%, -50%) rotate(-90deg);
+text-decoration: none;
+z-index: 1;
+`
+
+const BottomBar = styled.div`
+position: absolute;
+bottom: 1rem;
+left: 0;
+right: 0;
+width: 100%;
+
+display: flex;
+justify-content: space-evenly;
+`
+const ABOUT = styled(NavLink)`
+color: ${props => props.theme.text};
+text-decoration: none;
+z-index: 1;
+`
+
+const SKILLS = styled(NavLink)`
+color: ${props => props.theme.text};
+text-decoration: none;
+z-index: 1;
+`
+
+const rotate = keyframes`
+from{
+    transform: rotate(0);
+}
+to{
+    transform: rotate(360deg);
+}
+`
+
+const Center = styled.button`
+position: absolute;
+top: ${props => props.click ? '85%' : '50%'};
+left: ${props => props.click ? '92%' : '50%'};
+transform: translate(-50%, -50%);
+border: none;
+outline: none;
+background-color: transparent;
+cursor: pointer;
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+&>:first-child{
+    animation: ${rotate} infinite 1.5s linear;
+}
+
+&>:last-child{
+    padding-top: 1rem;
+}
+`
+
+const Main = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
         <MainContainer>
             <Container>
-            <PowerButton/>
-            <LogoComponent/>
+                <PowerButton />
+                <LogoComponent />
+                <SocialIcons />
+
+                <Center click={click}>
+                    <YinYang onClick={()=> handleClick()} width={200} height={200} fill='currentColor'/>
+                    <span>Click here</span>
+                </Center>
+
+                <Contact target="_blank" to={{ pathname: "mailto:devildevil50100@gmail.com" }}>
+                    <h2>
+                        Say hi..
+                    </h2>
+                </Contact>
+
+                <BLOG to="/blog">
+                    <h2>
+                        Blog
+                    </h2>
+                </BLOG>
+
+                <WORK to="/work">
+                    <h2>
+                        Work
+                    </h2>
+                </WORK>
+
+                <BottomBar>
+                    <ABOUT to="/about">
+                        <h2>
+                            About.
+                        </h2>
+                    </ABOUT>
+                    <SKILLS to="/skills">
+                        <h2>
+                            My Skills.
+                        </h2>
+                    </SKILLS>
+                </BottomBar>
+
             </Container>
         </MainContainer>
     )
